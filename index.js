1231aa3d874c11ffffa5614cc512c392c5b92608
Marcin Spasinski <marcin7789@gmail.com>
2020-09-25 15:03:06 +0200

    play from yt, commands to turn on and off

--- index.js
+++ index.js
@@ -1,44 +1,92 @@
 const dotenv = require("dotenv").config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const ytdl = require("ytdl-core");
+
+var isBotOn = [];
 
 client.once("ready", () => {
   console.log("Ready!");
 });
 
 client.on("message", (message) => {
-  if (message.content === "!ping") {
-    message.channel.send("Pong.");
+  let text = message.content;
+  switch (text) {
+    case "!uncleon":
+      message.channel.send(`Wujek przywita dla jego!`);
+      setBotActivity(message.member.guild.id, true);
+      break;
+    case "!uncleoff":
+      message.channel.send(`Wujek nie będzie witać!`);
+      setBotActivity(message.member.guild.id, false);
+      break;
+    default:
+      return true;
   }
 });
 
-client.on("voiceStateUpdate", (oldMember, newMember) => {
-  const newUserChannel = newMember.channelID;
-  const oldUserChannel = oldMember.channelID;
+const setBotActivity = (guildId, value) => {
+  let found = false;
 
-  if (newUserChannel === "758673613095174189") {
-    const channel = client.channels.cache.get(newUserChannel);
-    if (!channel) return console.error("The channel does not exist!");
-    channel
-      .join()
-      .then((connection) => {
-        const dispatcher = new Promise((resolve, reject) => {
-          connection.play("./witam.mp3");
-          setTimeout(() => resolve("done"), 8000);
-        }).then(() => {
-          channel.leave();
+  for (let i = 0; i < isBotOn.length; i++) {
+    const element = isBotOn[i];
+    if (element.id === guildId) {
+      element.value = value;
+      found = true;
+    }
+  }
+
+  if (!found) isBotOn.push({ id: guildId, value: value });
+};
+
+const checkIfBotIsOn = (guildId) => {
+  for (let i = 0; i < isBotOn.length; i++) {
+    const element = isBotOn[i];
+    if (element.id === guildId) return element.value;
+  }
+
+  isBotOn.push({ id: guildId, value: true });
+  return true;
+};
+
+client.on("voiceStateUpdate", (oldMember, newMember) => {
+  let isMyBotOn = checkIfBotIsOn(newMember.guild.id);
+  if (isMyBotOn) {
+    const newUserChannel = newMember.channelID;
+    const oldUserChannel = oldMember.channelID;
+
+    const greetingsUrl =
+      newMember.id === "305704741667602443"
+        ? "https://youtu.be/dDnR-l6mQ9c"
+        : "https://youtu.be/yhgSV9OgPJQ"; //kwas
+
+    const video = ytdl(greetingsUrl);
+
+    if (oldUserChannel !== newUserChannel && newUserChannel) {
+      const channel = client.channels.cache.get(newUserChannel);
+      if (!channel) return console.error("The channel does not exist!");
+      channel
+        .join()
+        .then((connection) => {
+          const dispatcher = connection.play(video, { volume: 1.0 });
+          dispatcher.on("finish", () => {
+            channel.leave();
+          });
+        })
+        .catch((e) => {
+          console.error(e);
         });
-      })
-      .catch((e) => {
-        // Oh no, it errored! Let's log it to console :)
-        console.error(e);
-      });
-  } else if (
-    oldUserChannel === "758673613095174189" &&
-    newUserChannel !== "758673613095174189"
-  ) {
-    console.log(`has left the channel`);
+    }
   }
 });
 
 client.login(process.env.BOT_TOKEN);
+
+/*if (
+  oldUserChannel !== newUserChannel &&
+  (newUserChannel === "700758865893130260" ||
+    newUserChannel === "377511754760192001" ||
+    newUserChannel === "378644418686877698")
+) */
+
+//if (oldUserChannel !== newUserChannel && newUserChannel === "758673613095174189")
